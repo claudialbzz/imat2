86255a2ac9d660e63695d47228e05e6254d1d1b6
claudialbzz <claudialbombin@gmail.com>
2025-09-11 09:15:15 +0200

    Cambio y final de carpetas

--- Adquisicion/P1/search_main.py
+++ Adquisicion/P1/search_main.py
@@ -1,26 +1,39 @@
 class AFD:
-    def __init__(self, estados, alfabeto, transiciones, estado_inicial, estados_finales):
-        self.estados = estados
-        self.alfabeto = alfabeto
-        self.transiciones = transiciones # Diccionario de diccionarios: {estado: {letra: siguiente_estado}} OJO: hay que incluir todos los simbolos del diccionario y sus transiciones(si no no funciona xd)
-        self.estado_inicial = estado_inicial
-        self.estados_finales = estados_finales
+	def __init__(self, estados, alfabeto, transiciones, estado_inicial, estados_finales):
+		self.estados = estados
+		self.alfabeto = alfabeto
+		self.transiciones = transiciones # Diccionario de diccionarios: {estado: {letra: siguiente_estado}} OJO: hay que incluir todos los simbolos del diccionario y sus transiciones(si no no funciona xd)
+		self.estado_inicial = estado_inicial
+		self.estados_finales = estados_finales
 
-    def procesar_cadena(self, cadena):
-        estado_actual = self.estado_inicial
-        for simbolo in cadena:
-            if estado_actual in self.transiciones and simbolo in self.transiciones[estado_actual]:
-                estado_actual = self.transiciones[estado_actual][simbolo]
-            else:
-                return False # Símbolo no válido o transición inexistente
-
-        return estado_actual in self.estados_finales
+	def procesar_cadena(self, cadena):
+		ocurrencias = 0
+		estado_actual = self.estado_inicial
+		for simbolo in cadena:
+			if estado_actual in self.transiciones and simbolo in self.transiciones[estado_actual]:
+				estado_actual = self.transiciones[estado_actual][simbolo]
+			else:
+				return False # Símbolo no válido o transición inexistente
+			if estado_actual == 'q2': # Si llegamos al estado final, contamos una ocurrencia y volvemos al estado inicial
+				ocurrencias += 1
+		print(f"La cadena '{cadena}' contiene la subcadena buscada {ocurrencias} veces.")
+		return estado_actual in self.estados_finales
 
 # Un AFD que acepta cadenas que conjetngan -ajo- 
 estados = {'q0', 'q1', 'q2'}
 alfabeto = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '}
 
+# Solicitamos al usuario que ingrese la cadena que desea buscar
+buscar = str(input("Introduce la cadena que deseas buscar (por ejemplo, 'ajo'): "))
+
+# Creamos una lista con los simbolos del alfabeto que aparecen en la cadena a buscar
+caracteres_buscar = []
+for i in buscar:
+    if i not in caracteres_buscar:
+    	caracteres_buscar.append(i)
+    
 # Transiciones: {estado_actual: {símbolo_entrada: siguiente_estado}}
+# Aqui necesitamso manualmente introducir las transiciones dependiendo de la cadena a buscar que hayamos introducido
 transiciones = {
     'q0': {'a': 'q1', 'b': 'q0', 'c': 'q0', 'd': 'q0', 'e': 'q0', 'f': 'q0', 'g': 'q0', 'h': 'q0', 'i': 'q0', 'j': 'q1', 'k': 'q0', 'l': 'q0', 'm': 'q0', 'n': 'q0', 'o': 'q0', 'p': 'q0', 'q': 'q0', 'r': 'q0', 's': 'q0', 't': 'q0', 'u': 'q0', 'v': 'q0', 'w': 'q0', 'x': 'q0', 'y': 'q0', 'z': 'q0', ' ': 'q0'},
     'q1': {'j': 'q1', 'a': 'q1', 'b': 'q0', 'c': 'q0', 'd': 'q0', 'e': 'q0', 'f': 'q0', 'g': 'q0', 'h': 'q0', 'i': 'q0', 'k': 'q0', 'l': 'q0', 'm': 'q0', 'n': 'q0', 'o': 'q2', 'p': 'q0', 'q': 'q0', 'r': 'q0', 's': 'q0', 't': 'q0', 'u': 'q0', 'v': 'q0', 'w': 'q0', 'x': 'q0', 'y': 'q0', 'z': 'q0', ' ': 'q0'},
